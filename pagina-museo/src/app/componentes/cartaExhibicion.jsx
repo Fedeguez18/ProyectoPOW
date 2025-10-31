33a35521e259e4618de5b0a0a0a56ac993ef3f9a
Yaralopez12 <yaraailinlopez@gmail.com>
2025-10-31 17:39:34 -0300

    api imagenes

--- pagina-museo/src/app/componentes/cartaExhibicion.jsx
+++ pagina-museo/src/app/componentes/cartaExhibicion.jsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from "next/image";
 import Link from "next/link";
-import styles from "../styles/cartaEXhibiciones.module.css";
+import styles from "../styles/cartaExhibiciones.module.css";
 
 export default function CartaEXhibiciones({ item }) {
     return (
